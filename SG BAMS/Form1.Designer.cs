4aa0b78a1c2a46683b75822b0b9623e044b28447
B.V <B.V@ACER-NITRO-V15>
2026-02-09 21:11:32 -0600

    BOTON

--- SG BAMS/Form1.Designer.cs
+++ SG BAMS/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -41,11 +42,21 @@
             label1.TabIndex = 0;
             label1.Text = "BAMS";
             // 
+            // button1
+            // 
+            button1.Location = new Point(333, 279);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "HOLA";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -56,5 +67,6 @@
         #endregion
 
         private Label label1;
+        private Button button1;
     }
 }
