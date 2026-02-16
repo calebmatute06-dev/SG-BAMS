642feb4dc8c354e2cc7c0f758e1677ddc7d77d2c
Josué Mendoza <xyxscorpionxyx@gmail.com>
2026-02-15 22:20:22 -0600

    Agregue formulario de administración

---
+++ SG BAMS/frmAdministracion.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace SG_BAMS
+{
+    partial class frmAdministracion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            button3 = new Button();
+            label4 = new Label();
+            button4 = new Button();
+            label5 = new Label();
+            button5 = new Button();
+            label6 = new Label();
+            button6 = new Button();
+            label7 = new Label();
+            button7 = new Button();
+            label8 = new Label();
+            button8 = new Button();
+            label9 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.SkyBlue;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(257, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(245, 29);
+            label1.TabIndex = 0;
+            label1.Text = "Administración de BAMS";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 9F);
+            label2.ForeColor = Color.Navy;
+            label2.Location = new Point(233, 120);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Agregar Usuarios";
+            label2.Click += label2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SkyBlue;
+            button1.ForeColor = Color.Navy;
+            button1.Location = new Point(441, 116);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Ver";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.SkyBlue;
+            button2.ForeColor = Color.Navy;
+            button2.Location = new Point(441, 145);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "Ver";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.ForeColor = Color.Navy;
+            label3.Location = new Point(233, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Rol de Usuario";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.SkyBlue;
+            button3.ForeColor = Color.Navy;
+            button3.Location = new Point(441, 174);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Ver";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.ForeColor = Color.Navy;
+            label4.Location = new Point(233, 178);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Tipo producto";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.SkyBlue;
+            button4.ForeColor = Color.Navy;
+            button4.Location = new Point(441, 203);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 8;
+            button4.Text = "Ver";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.ForeColor = Color.Navy;
+            label5.Location = new Point(233, 207);
+            label5.Name = "label5";
+            label5.Size = new Size(130, 15);
+            label5.TabIndex = 7;
+            label5.Text = "Tipo de Forma de Pago";
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.SkyBlue;
+            button5.ForeColor = Color.Navy;
+            button5.Location = new Point(441, 232);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 10;
+            button5.Text = "Ver";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.ForeColor = Color.Navy;
+            label6.Location = new Point(233, 236);
+            label6.Name = "label6";
+            label6.Size = new Size(42, 15);
+            label6.TabIndex = 9;
+            label6.Text = "Estado";
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.SkyBlue;
+            button6.ForeColor = Color.Navy;
+            button6.Location = new Point(441, 261);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 12;
+            button6.Text = "Ver";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.ForeColor = Color.Navy;
+            label7.Location = new Point(233, 265);
+            label7.Name = "label7";
+            label7.Size = new Size(113, 15);
+            label7.TabIndex = 11;
+            label7.Text = "Marca de Productos";
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.SkyBlue;
+            button7.ForeColor = Color.Navy;
+            button7.Location = new Point(441, 290);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 14;
+            button7.Text = "Ver";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.ForeColor = Color.Navy;
+            label8.Location = new Point(233, 294);
+            label8.Name = "label8";
+            label8.Size = new Size(91, 15);
+            label8.TabIndex = 13;
+            label8.Text = "Modelo de auto";
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.SkyBlue;
+            button8.ForeColor = Color.Navy;
+            button8.Location = new Point(521, 404);
+            button8.Name = "button8";
+            button8.Size = new Size(78, 23);
+            button8.TabIndex = 15;
+            button8.Text = "Salir";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Segoe UI", 15F);
+            label9.ForeColor = Color.Navy;
+            label9.Location = new Point(605, 414);
+            label9.Name = "label9";
+            label9.Size = new Size(65, 28);
+            label9.TabIndex = 16;
+            label9.Text = "BAMS";
+            // 
+            // frmAdministracion
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(682, 450);
+            Controls.Add(label9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(label8);
+            Controls.Add(button6);
+            Controls.Add(label7);
+            Controls.Add(button5);
+            Controls.Add(label6);
+            Controls.Add(button4);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmAdministracion";
+            Text = "frmAdministracion";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private Button button3;
+        private Label label4;
+        private Button button4;
+        private Label label5;
+        private Button button5;
+        private Label label6;
+        private Button button6;
+        private Label label7;
+        private Button button7;
+        private Label label8;
+        private Button button8;
+        private Label label9;
+    }
+}
