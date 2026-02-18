baf75b70d5e9c61dbe1310c4b53d8d13b98d30c5
Josué Mendoza <xyxscorpionxyx@gmail.com>
2026-02-17 22:56:37 -0600

    x

--- SG BAMS/fmrAgregarModeloAuto.Designer.cs
+++ SG BAMS/fmrAgregarModeloAuto.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace SG_BAMS
+﻿
+namespace SG_BAMS
 {
     partial class fmrAgregarModeloAuto
     {
@@ -227,6 +228,11 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
             ResumeLayout(false);
             PerformLayout();
+        }
+
+        private void kryptonButton7_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
