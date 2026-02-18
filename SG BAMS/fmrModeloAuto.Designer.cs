d031c1b4409fef5462a8739715186f87062a276f
Josué Mendoza <xyxscorpionxyx@gmail.com>
2026-02-17 22:30:37 -0600

    formularios de Modelo de auto

---
+++ SG BAMS/fmrModeloAuto.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace SG_BAMS
+{
+    partial class fmrModeloAuto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmrModeloAuto));
+            label7 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            panel2 = new Panel();
+            kryptonDataGridView1 = new Krypton.Toolkit.KryptonDataGridView();
+            pictureBox16 = new PictureBox();
+            label1 = new Label();
+            kryptonButton2 = new Krypton.Toolkit.KryptonButton();
+            kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonDataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).BeginInit();
+            SuspendLayout();
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial Narrow", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Navy;
+            label7.Location = new Point(513, 340);
+            label7.Name = "label7";
+            label7.Size = new Size(60, 25);
+            label7.TabIndex = 102;
+            label7.Text = "BAMS";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Navy;
+            pictureBox1.Location = new Point(2, 4);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(21, 381);
+            pictureBox1.TabIndex = 101;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Navy;
+            panel1.Location = new Point(2, 2);
+            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(598, 18);
+            panel1.TabIndex = 99;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Navy;
+            pictureBox2.Location = new Point(579, 2);
+            pictureBox2.Margin = new Padding(3, 2, 3, 2);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(21, 383);
+            pictureBox2.TabIndex = 100;
+            pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Navy;
+            panel2.Location = new Point(2, 367);
+            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(598, 18);
+            panel2.TabIndex = 98;
+            // 
+            // kryptonDataGridView1
+            // 
+            kryptonDataGridView1.AllowUserToAddRows = false;
+            kryptonDataGridView1.AllowUserToDeleteRows = false;
+            kryptonDataGridView1.BorderStyle = BorderStyle.None;
+            kryptonDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            kryptonDataGridView1.Location = new Point(69, 91);
+            kryptonDataGridView1.Name = "kryptonDataGridView1";
+            kryptonDataGridView1.ReadOnly = true;
+            kryptonDataGridView1.Size = new Size(448, 174);
+            kryptonDataGridView1.StateCommon.Background.Color1 = Color.SkyBlue;
+            kryptonDataGridView1.StateCommon.Background.Color2 = Color.SkyBlue;
+            kryptonDataGridView1.StateCommon.BackStyle = Krypton.Toolkit.PaletteBackStyle.GridBackgroundList;
+            kryptonDataGridView1.TabIndex = 97;
+            // 
+            // pictureBox16
+            // 
+            pictureBox16.BackgroundImage = (Image)resources.GetObject("pictureBox16.BackgroundImage");
+            pictureBox16.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox16.Location = new Point(352, 38);
+            pictureBox16.Margin = new Padding(3, 2, 3, 2);
+            pictureBox16.Name = "pictureBox16";
+            pictureBox16.Size = new Size(35, 29);
+            pictureBox16.TabIndex = 96;
+            pictureBox16.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Arial Narrow", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(157, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(245, 29);
+            label1.TabIndex = 95;
+            label1.Text = "Modelo de auto";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // kryptonButton2
+            // 
+            kryptonButton2.Location = new Point(322, 280);
+            kryptonButton2.Name = "kryptonButton2";
+            kryptonButton2.Size = new Size(121, 45);
+            kryptonButton2.StateCommon.Content.Image.ImageH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonButton2.StateCommon.Content.ShortText.Color1 = Color.Navy;
+            kryptonButton2.StateCommon.Content.ShortText.Font = new Font("Arial Narrow", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            kryptonButton2.StateNormal.Back.Color1 = Color.SkyBlue;
+            kryptonButton2.StateNormal.Back.Color2 = Color.SkyBlue;
+            kryptonButton2.StateNormal.Border.Rounding = 15F;
+            kryptonButton2.TabIndex = 94;
+            kryptonButton2.Values.DropDownArrowColor = Color.Empty;
+            kryptonButton2.Values.Text = "Modficar";
+            // 
+            // kryptonButton1
+            // 
+            kryptonButton1.Location = new Point(94, 280);
+            kryptonButton1.Name = "kryptonButton1";
+            kryptonButton1.Size = new Size(121, 45);
+            kryptonButton1.StateCommon.Content.Image.ImageH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            kryptonButton1.StateCommon.Content.ShortText.Color1 = Color.Navy;
+            kryptonButton1.StateCommon.Content.ShortText.Font = new Font("Arial Narrow", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            kryptonButton1.StateNormal.Back.Color1 = Color.SkyBlue;
+            kryptonButton1.StateNormal.Back.Color2 = Color.SkyBlue;
+            kryptonButton1.StateNormal.Border.Rounding = 15F;
+            kryptonButton1.TabIndex = 93;
+            kryptonButton1.Values.DropDownArrowColor = Color.Empty;
+            kryptonButton1.Values.Text = "Agregar";
+            // 
+            // fmrModeloAuto
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(602, 388);
+            Controls.Add(label7);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(panel2);
+            Controls.Add(kryptonDataGridView1);
+            Controls.Add(pictureBox16);
+            Controls.Add(label1);
+            Controls.Add(kryptonButton2);
+            Controls.Add(kryptonButton1);
+            Name = "fmrModeloAuto";
+            Text = "fmrModeloAuto";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonDataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label7;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private PictureBox pictureBox2;
+        private Panel panel2;
+        private Krypton.Toolkit.KryptonDataGridView kryptonDataGridView1;
+        private PictureBox pictureBox16;
+        private Label label1;
+        private Krypton.Toolkit.KryptonButton kryptonButton2;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+    }
+}
