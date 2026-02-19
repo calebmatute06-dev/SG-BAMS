224725a718f7d3e20520056ce17dcb6b70ec0439
B.V <B.V@ACER-NITRO-V15>
2026-02-19 09:41:00 -0600

    porceso fallido

--- SG BAMS/CampoNoEncontrado.Designer.cs
+++ SG BAMS/CampoNoEncontrado.Designer.cs
@@ -144,7 +144,7 @@
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(515, 216);
+            ClientSize = new Size(514, 216);
             Controls.Add(btnsalirLogin);
             Controls.Add(label2);
             Controls.Add(label1);
