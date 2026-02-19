c552cbeafc4e7f869cf7965d8260b33c90027751
Caleb Matute <calebmatute06@gmail.com>
2026-02-19 00:47:33 -0600

    x

--- SG BAMS/MenuPrincipalEmp.cs
+++ SG BAMS/MenuPrincipalEmp.cs
@@ -1,5 +1,4 @@
-﻿using Krypton.Toolkit;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +17,9 @@ namespace SG_BAMS
             InitializeComponent();
         }
 
-        
+        private void btnCerrarSesion_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
