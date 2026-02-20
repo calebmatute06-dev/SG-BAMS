399b2566d0ac0ab236ed333580ee60b521122a8e
B.V <B.V@ACER-NITRO-V15>
2026-02-19 21:10:40 -0600

    dsadas

--- SG BAMS/ModificarProducto.Designer.cs
+++ SG BAMS/ModificarProducto.Designer.cs
@@ -114,7 +114,6 @@
             btnCancelar.TabIndex = 208;
             btnCancelar.Values.DropDownArrowColor = Color.Empty;
             btnCancelar.Values.Text = "Cancelar";
-            //btnCancelar.Click += this.kryptonButton1_Click;
             // 
             // btnAceptar
             // 
@@ -173,7 +172,6 @@
             btnsalir.TabIndex = 205;
             btnsalir.Values.DropDownArrowColor = Color.Empty;
             btnsalir.Values.Text = "Salir";
-            //btnsalir.Click += this.btnsalir_Click;
             // 
             // pictureBox3
             // 
@@ -195,7 +193,7 @@
             // 
             // txtStock
             // 
-            txtStock.Location = new Point(205, 472);
+            txtStock.Location = new Point(204, 468);
             txtStock.Name = "txtStock";
             txtStock.Size = new Size(250, 39);
             txtStock.StateCommon.Back.Color1 = Color.SkyBlue;
@@ -209,7 +207,7 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Navy;
-            label1.Location = new Point(70, 422);
+            label1.Location = new Point(69, 417);
             label1.Name = "label1";
             label1.Size = new Size(91, 33);
             label1.TabIndex = 222;
@@ -217,7 +215,7 @@
             // 
             // txtPrecio
             // 
-            txtPrecio.Location = new Point(205, 198);
+            txtPrecio.Location = new Point(204, 190);
             txtPrecio.Name = "txtPrecio";
             txtPrecio.Size = new Size(250, 39);
             txtPrecio.StateCommon.Back.Color1 = Color.SkyBlue;
@@ -226,7 +224,7 @@
             // 
             // txtNombre
             // 
-            txtNombre.Location = new Point(205, 145);
+            txtNombre.Location = new Point(204, 137);
             txtNombre.Name = "txtNombre";
             txtNombre.Size = new Size(250, 39);
             txtNombre.StateCommon.Back.Color1 = Color.SkyBlue;
@@ -236,7 +234,7 @@
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(205, 96);
+            txtUsuario.Location = new Point(204, 88);
             txtUsuario.Name = "txtUsuario";
             txtUsuario.Size = new Size(250, 39);
             txtUsuario.StateCommon.Back.Color1 = Color.SkyBlue;
@@ -249,12 +247,11 @@
             label15.BackColor = Color.Transparent;
             label15.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label15.ForeColor = Color.Navy;
-            label15.Location = new Point(70, 478);
+            label15.Location = new Point(69, 474);
             label15.Name = "label15";
             label15.Size = new Size(76, 33);
             label15.TabIndex = 218;
             label15.Text = "Stock:";
-            //label15.Click += this.label15_Click;
             // 
             // label10
             // 
@@ -262,7 +259,7 @@
             label10.BackColor = Color.Transparent;
             label10.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label10.ForeColor = Color.Navy;
-            label10.Location = new Point(70, 368);
+            label10.Location = new Point(69, 360);
             label10.Name = "label10";
             label10.Size = new Size(149, 33);
             label10.TabIndex = 217;
@@ -274,7 +271,7 @@
             label12.BackColor = Color.Transparent;
             label12.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label12.ForeColor = Color.Navy;
-            label12.Location = new Point(70, 311);
+            label12.Location = new Point(69, 303);
             label12.Name = "label12";
             label12.Size = new Size(71, 33);
             label12.TabIndex = 216;
@@ -286,7 +283,7 @@
             label14.BackColor = Color.Transparent;
             label14.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label14.ForeColor = Color.Navy;
-            label14.Location = new Point(70, 256);
+            label14.Location = new Point(69, 248);
             label14.Name = "label14";
             label14.Size = new Size(89, 33);
             label14.TabIndex = 215;
@@ -298,7 +295,7 @@
             label7.BackColor = Color.Transparent;
             label7.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label7.ForeColor = Color.Navy;
-            label7.Location = new Point(70, 204);
+            label7.Location = new Point(69, 196);
             label7.Name = "label7";
             label7.Size = new Size(85, 33);
             label7.TabIndex = 214;
@@ -310,7 +307,7 @@
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.Navy;
-            label4.Location = new Point(70, 151);
+            label4.Location = new Point(69, 143);
             label4.Name = "label4";
             label4.Size = new Size(111, 33);
             label4.TabIndex = 213;
@@ -322,7 +319,7 @@
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Arial Narrow", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.Navy;
-            label3.Location = new Point(70, 102);
+            label3.Location = new Point(69, 94);
             label3.Name = "label3";
             label3.Size = new Size(50, 33);
             label3.TabIndex = 212;
@@ -331,7 +328,7 @@
             // cmbMarca
             // 
             cmbMarca.DropDownWidth = 300;
-            cmbMarca.Location = new Point(205, 243);
+            cmbMarca.Location = new Point(204, 235);
             cmbMarca.Name = "cmbMarca";
             cmbMarca.Size = new Size(250, 52);
             cmbMarca.StateCommon.ComboBox.Back.Color1 = Color.SkyBlue;
@@ -345,7 +342,7 @@
             // cmbTipo
             // 
             cmbTipo.DropDownWidth = 300;
-            cmbTipo.Location = new Point(205, 301);
+            cmbTipo.Location = new Point(204, 293);
             cmbTipo.Name = "cmbTipo";
             cmbTipo.Size = new Size(250, 52);
             cmbTipo.StateCommon.ComboBox.Back.Color1 = Color.SkyBlue;
@@ -359,7 +356,7 @@
             // cmbModelo
             // 
             cmbModelo.DropDownWidth = 300;
-            cmbModelo.Location = new Point(205, 359);
+            cmbModelo.Location = new Point(204, 351);
             cmbModelo.Name = "cmbModelo";
             cmbModelo.Size = new Size(250, 52);
             cmbModelo.StateCommon.ComboBox.Back.Color1 = Color.SkyBlue;
@@ -373,7 +370,7 @@
             // cmbEstado
             // 
             cmbEstado.DropDownWidth = 300;
-            cmbEstado.Location = new Point(205, 414);
+            cmbEstado.Location = new Point(204, 409);
             cmbEstado.Name = "cmbEstado";
             cmbEstado.Size = new Size(250, 52);
             cmbEstado.StateCommon.ComboBox.Back.Color1 = Color.SkyBlue;
