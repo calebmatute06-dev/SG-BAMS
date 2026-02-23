288b4295c67b3f8d670f16a969df76b80f43d81a
Caleb Matute <calebmatute06@gmail.com>
2026-02-23 17:07:42 -0600

    x

--- SG BAMS/Facturas/FacturaAgregarDatos.Designer.cs
+++ SG BAMS/Facturas/FacturaAgregarDatos.Designer.cs
@@ -70,35 +70,47 @@
             kryptonDataGridView1.BorderStyle = BorderStyle.None;
             kryptonDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             kryptonDataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
-            kryptonDataGridView1.Location = new Point(38, 155);
+            kryptonDataGridView1.Location = new Point(43, 207);
+            kryptonDataGridView1.Margin = new Padding(3, 4, 3, 4);
             kryptonDataGridView1.Name = "kryptonDataGridView1";
-            kryptonDataGridView1.Size = new Size(548, 150);
+            kryptonDataGridView1.RowHeadersWidth = 51;
+            kryptonDataGridView1.Size = new Size(626, 200);
             kryptonDataGridView1.TabIndex = 310;
             // 
             // Column1
             // 
             Column1.HeaderText = "Codigo";
+            Column1.MinimumWidth = 6;
             Column1.Name = "Column1";
+            Column1.Width = 125;
             // 
             // Column2
             // 
             Column2.HeaderText = "Producto";
+            Column2.MinimumWidth = 6;
             Column2.Name = "Column2";
+            Column2.Width = 125;
             // 
             // Column3
             // 
             Column3.HeaderText = "Cantidad";
+            Column3.MinimumWidth = 6;
             Column3.Name = "Column3";
+            Column3.Width = 125;
             // 
             // Column4
             // 
             Column4.HeaderText = "Precio";
+            Column4.MinimumWidth = 6;
             Column4.Name = "Column4";
+            Column4.Width = 125;
             // 
             // Column5
             // 
             Column5.HeaderText = "SubT";
+            Column5.MinimumWidth = 6;
             Column5.Name = "Column5";
+            Column5.Width = 125;
             // 
             // label7
             // 
@@ -106,9 +118,9 @@
             label7.BackColor = Color.Transparent;
             label7.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label7.ForeColor = Color.Navy;
-            label7.Location = new Point(698, 115);
+            label7.Location = new Point(798, 153);
             label7.Name = "label7";
-            label7.Size = new Size(56, 19);
+            label7.Size = new Size(67, 24);
             label7.TabIndex = 307;
             label7.Text = "Fecha";
             // 
@@ -118,9 +130,9 @@
             label9.BackColor = Color.Transparent;
             label9.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label9.ForeColor = Color.Navy;
-            label9.Location = new Point(53, 327);
+            label9.Location = new Point(61, 436);
             label9.Name = "label9";
-            label9.Size = new Size(125, 19);
+            label9.Size = new Size(153, 24);
             label9.TabIndex = 304;
             label9.Text = "Forma de Pago";
             // 
@@ -130,9 +142,9 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Navy;
-            label1.Location = new Point(53, 126);
+            label1.Location = new Point(61, 168);
             label1.Name = "label1";
-            label1.Size = new Size(104, 19);
+            label1.Size = new Size(128, 24);
             label1.TabIndex = 303;
             label1.Text = "Bateria Vieja";
             // 
@@ -142,9 +154,9 @@
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.Navy;
-            label3.Location = new Point(53, 93);
+            label3.Location = new Point(61, 124);
             label3.Name = "label3";
-            label3.Size = new Size(62, 19);
+            label3.Size = new Size(75, 24);
             label3.TabIndex = 302;
             label3.Text = "Cliente";
             // 
@@ -154,9 +166,9 @@
             label5.BackColor = Color.Transparent;
             label5.Font = new Font("Arial Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label5.ForeColor = Color.Navy;
-            label5.Location = new Point(771, 394);
+            label5.Location = new Point(881, 525);
             label5.Name = "label5";
-            label5.Size = new Size(93, 33);
+            label5.Size = new Size(113, 42);
             label5.TabIndex = 299;
             label5.Text = "BAMS";
             // 
@@ -166,68 +178,62 @@
             label4.BackColor = Color.SkyBlue;
             label4.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = Color.Navy;
-            label4.Location = new Point(38, 37);
+            label4.Location = new Point(43, 49);
             label4.Name = "label4";
-            label4.Size = new Size(177, 29);
+            label4.Size = new Size(223, 35);
             label4.TabIndex = 296;
             label4.Text = "Nueva Factura";
             // 
             // panel3
             // 
             panel3.BackColor = Color.Navy;
-            panel3.Location = new Point(870, -2);
-            panel3.Margin = new Padding(3, 2, 3, 2);
+            panel3.Location = new Point(994, -3);
             panel3.Name = "panel3";
-            panel3.Size = new Size(22, 449);
+            panel3.Size = new Size(25, 599);
             panel3.TabIndex = 294;
             // 
             // panel1
             // 
             panel1.BackColor = Color.Navy;
-            panel1.Location = new Point(21, -2);
-            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Location = new Point(24, -3);
             panel1.Name = "panel1";
-            panel1.Size = new Size(858, 18);
+            panel1.Size = new Size(981, 24);
             panel1.TabIndex = 293;
             // 
             // panel4
             // 
             panel4.BackColor = Color.Navy;
-            panel4.Location = new Point(3, 429);
-            panel4.Margin = new Padding(3, 2, 3, 2);
+            panel4.Location = new Point(3, 572);
             panel4.Name = "panel4";
-            panel4.Size = new Size(876, 18);
+            panel4.Size = new Size(1001, 24);
             panel4.TabIndex = 295;
             // 
             // panel2
             // 
             panel2.BackColor = Color.Navy;
-            panel2.Location = new Point(6, 237);
-            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Location = new Point(7, 316);
             panel2.Name = "panel2";
-            panel2.Size = new Size(562, 18);
+            panel2.Size = new Size(642, 24);
             panel2.TabIndex = 236;
             // 
             // panel8
             // 
             panel8.BackColor = Color.Navy;
             panel8.Controls.Add(panel2);
-            panel8.Location = new Point(0, -3);
-            panel8.Margin = new Padding(3, 2, 3, 2);
+            panel8.Location = new Point(0, -4);
             panel8.Name = "panel8";
-            panel8.Size = new Size(22, 450);
+            panel8.Size = new Size(25, 600);
             panel8.TabIndex = 289;
             // 
             // kryptonButton12
             // 
-            kryptonButton12.Location = new Point(53, 365);
-            kryptonButton12.Margin = new Padding(3, 2, 3, 2);
+            kryptonButton12.Location = new Point(61, 487);
             kryptonButton12.Name = "kryptonButton12";
             kryptonButton12.OverrideDefault.Back.Color1 = Color.SkyBlue;
             kryptonButton12.OverrideDefault.Back.Color2 = Color.White;
             kryptonButton12.OverrideFocus.Back.Color1 = Color.SkyBlue;
             kryptonButton12.OverrideFocus.Back.Color2 = Color.White;
-            kryptonButton12.Size = new Size(128, 45);
+            kryptonButton12.Size = new Size(146, 60);
             kryptonButton12.StateCommon.Back.Color1 = Color.SkyBlue;
             kryptonButton12.StateCommon.Back.Color2 = Color.White;
             kryptonButton12.StateCommon.Border.Rounding = 30F;
@@ -243,14 +249,13 @@
             // 
             // kryptonButton1
             // 
-            kryptonButton1.Location = new Point(187, 365);
-            kryptonButton1.Margin = new Padding(3, 2, 3, 2);
+            kryptonButton1.Location = new Point(214, 487);
             kryptonButton1.Name = "kryptonButton1";
             kryptonButton1.OverrideDefault.Back.Color1 = Color.SkyBlue;
             kryptonButton1.OverrideDefault.Back.Color2 = Color.White;
             kryptonButton1.OverrideFocus.Back.Color1 = Color.SkyBlue;
             kryptonButton1.OverrideFocus.Back.Color2 = Color.White;
-            kryptonButton1.Size = new Size(128, 45);
+            kryptonButton1.Size = new Size(146, 60);
             kryptonButton1.StateCommon.Back.Color1 = Color.SkyBlue;
             kryptonButton1.StateCommon.Back.Color2 = Color.White;
             kryptonButton1.StateCommon.Border.Rounding = 30F;
@@ -266,14 +271,13 @@
             // 
             // kryptonButton2
             // 
-            kryptonButton2.Location = new Point(321, 365);
-            kryptonButton2.Margin = new Padding(3, 2, 3, 2);
+            kryptonButton2.Location = new Point(367, 487);
             kryptonButton2.Name = "kryptonButton2";
             kryptonButton2.OverrideDefault.Back.Color1 = Color.SkyBlue;
             kryptonButton2.OverrideDefault.Back.Color2 = Color.White;
             kryptonButton2.OverrideFocus.Back.Color1 = Color.SkyBlue;
             kryptonButton2.OverrideFocus.Back.Color2 = Color.White;
-            kryptonButton2.Size = new Size(128, 45);
+            kryptonButton2.Size = new Size(146, 60);
             kryptonButton2.StateCommon.Back.Color1 = Color.SkyBlue;
             kryptonButton2.StateCommon.Back.Color2 = Color.White;
             kryptonButton2.StateCommon.Border.Rounding = 30F;
@@ -289,15 +293,17 @@
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(619, 166);
+            monthCalendar1.Location = new Point(707, 221);
+            monthCalendar1.Margin = new Padding(10, 12, 10, 12);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 315;
             // 
             // kryptonGroupBox1
             // 
             kryptonGroupBox1.CaptionVisible = false;
-            kryptonGroupBox1.Location = new Point(592, 137);
-            kryptonGroupBox1.Size = new Size(272, 216);
+            kryptonGroupBox1.Location = new Point(677, 183);
+            kryptonGroupBox1.Margin = new Padding(3, 4, 3, 4);
+            kryptonGroupBox1.Size = new Size(311, 288);
             kryptonGroupBox1.StateCommon.Back.Color1 = Color.SkyBlue;
             kryptonGroupBox1.StateCommon.Border.Rounding = 50F;
             kryptonGroupBox1.TabIndex = 316;
@@ -308,44 +314,48 @@
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.Navy;
-            label2.Location = new Point(420, 330);
+            label2.Location = new Point(480, 440);
             label2.Name = "label2";
-            label2.Size = new Size(46, 19);
+            label2.Size = new Size(57, 24);
             label2.TabIndex = 302;
             label2.Text = "Total";
             // 
             // kryptonGroupBox3
             // 
             kryptonGroupBox3.CaptionVisible = false;
-            kryptonGroupBox3.Location = new Point(28, 28);
-            kryptonGroupBox3.Size = new Size(199, 49);
+            kryptonGroupBox3.Location = new Point(32, 37);
+            kryptonGroupBox3.Margin = new Padding(3, 4, 3, 4);
+            kryptonGroupBox3.Size = new Size(257, 65);
             kryptonGroupBox3.StateCommon.Back.Color1 = Color.SkyBlue;
             kryptonGroupBox3.StateCommon.Border.Rounding = 50F;
             kryptonGroupBox3.TabIndex = 317;
             // 
             // kryptonTextBox1
             // 
-            kryptonTextBox1.Location = new Point(177, 87);
+            kryptonTextBox1.Location = new Point(202, 116);
+            kryptonTextBox1.Margin = new Padding(3, 4, 3, 4);
             kryptonTextBox1.Name = "kryptonTextBox1";
-            kryptonTextBox1.Size = new Size(172, 29);
+            kryptonTextBox1.Size = new Size(197, 33);
             kryptonTextBox1.StateCommon.Back.Color1 = Color.SkyBlue;
             kryptonTextBox1.StateCommon.Border.Rounding = 10F;
             kryptonTextBox1.TabIndex = 321;
             // 
             // kryptonTextBox2
             // 
-            kryptonTextBox2.Location = new Point(177, 121);
+            kryptonTextBox2.Location = new Point(202, 161);
+            kryptonTextBox2.Margin = new Padding(3, 4, 3, 4);
             kryptonTextBox2.Name = "kryptonTextBox2";
-            kryptonTextBox2.Size = new Size(172, 29);
+            kryptonTextBox2.Size = new Size(197, 33);
             kryptonTextBox2.StateCommon.Back.Color1 = Color.SkyBlue;
             kryptonTextBox2.StateCommon.Border.Rounding = 10F;
             kryptonTextBox2.TabIndex = 321;
             // 
             // kryptonTextBox3
             // 
-            kryptonTextBox3.Location = new Point(472, 327);
+            kryptonTextBox3.Location = new Point(539, 436);
+            kryptonTextBox3.Margin = new Padding(3, 4, 3, 4);
             kryptonTextBox3.Name = "kryptonTextBox3";
-            kryptonTextBox3.Size = new Size(96, 29);
+            kryptonTextBox3.Size = new Size(110, 33);
             kryptonTextBox3.StateCommon.Back.Color1 = Color.SkyBlue;
             kryptonTextBox3.StateCommon.Border.Rounding = 10F;
             kryptonTextBox3.TabIndex = 321;
@@ -353,10 +363,9 @@
             // kryptonComboBox2
             // 
             kryptonComboBox2.DropDownWidth = 300;
-            kryptonComboBox2.Location = new Point(184, 319);
-            kryptonComboBox2.Margin = new Padding(3, 2, 3, 2);
+            kryptonComboBox2.Location = new Point(210, 425);
             kryptonComboBox2.Name = "kryptonComboBox2";
-            kryptonComboBox2.Size = new Size(128, 34);
+            kryptonComboBox2.Size = new Size(146, 38);
             kryptonComboBox2.StateCommon.ComboBox.Back.Color1 = Color.SkyBlue;
             kryptonComboBox2.StateCommon.ComboBox.Border.Rounding = 20F;
             kryptonComboBox2.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
@@ -367,10 +376,10 @@
             // 
             // FacturaAgregarDatos
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(891, 449);
+            ClientSize = new Size(1018, 599);
             Controls.Add(kryptonComboBox2);
             Controls.Add(kryptonTextBox3);
             Controls.Add(kryptonTextBox2);
@@ -393,6 +402,7 @@
             Controls.Add(panel4);
             Controls.Add(panel8);
             Controls.Add(kryptonGroupBox3);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "FacturaAgregarDatos";
             Text = "FacturaAgregarDatos";
             ((System.ComponentModel.ISupportInitialize)kryptonDataGridView1).EndInit();
