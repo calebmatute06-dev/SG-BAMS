f03fbd9f640bd6196eddf785408b265e9a9e129a
B.V <B.V@ACER-NITRO-V15>
2026-03-15 18:34:10 -0600

    x

--- SG BAMS/ProductoInventario/ClsVerProducto.cs
+++ SG BAMS/ProductoInventario/ClsVerProducto.cs
@@ -11,7 +11,7 @@ namespace SG_BAMS.ProductoInventario
         public DataTable MostrarProductosCompleto()
         {
             DataTable tabla = new DataTable();
-            string query = "SELECT * FROM Vista_Productos_Detallada";
+            string query = "SELECT * FROM Vista_Productos_Detallada ORDER BY [Stock] DESC";
             try
             {
                 conexion.AbrirConexion();
