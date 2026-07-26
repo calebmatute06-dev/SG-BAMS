ed6c4b162351ef2ce55b0231af3522b60d66a71d
JosueMendoza375 <josuemendoza7528@gmail.com>
2026-07-26 01:13:01 -0600

    Xasd

---
+++ IChartBuilderService.cs
@@ -0,0 +1,15 @@
+using System.Data;
+using System.Threading.Tasks;
+
+namespace SG_BAMS.MenuPrincipal
+{
+    /// <summary>
+    /// Contrato para construcción de gráficos.
+    /// No expone tipos de UI para evitar dependencias entre ensamblados con TFMs distintos.
+    /// </summary>
+    public interface IChartBuilderService
+    {
+        Task CargarGraficoStock(object chartHost, DataTable tablaStock);
+        Task CargarGraficoMasVendidos(object chartHost, DataTable datosVentas);
+    }
+}
