6e8bfc29de78d79736c12b5004efe477f5c432af
calebmatute06-dev <calebmatute06@gmail.com>
2026-07-27 16:44:40 -0600

    arreglo de navy

--- SG BAMS/PlaceholderComboBox.cs
+++ SG BAMS/PlaceholderComboBox.cs
@@ -21,7 +21,7 @@ namespace SG_BAMS
         private bool _isLoading;
 
         private static readonly Color ColorPlaceholder = Color.Gray;
-        private static readonly Color ColorTextoNormal = Color.Black;
+        private static readonly Color ColorTextoNormal = Color.Navy;
 
         /// <summary>
         /// Inicializa el adaptador vinculando el KryptonComboBox con su texto de placeholder.
